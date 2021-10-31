fbac85b0e3b4bc1637196c49c08d41a839d6228f
TWENTeam <93426429+TWENTeam@users.noreply.github.com>
2021-11-01 03:28:18 +0500

    Custom Functions
    Custom functions added to all project

---
+++ includes/ajax.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Ajax related functions
+ *
+ * @link https://codex.wordpress.org/AJAX#Ajax_in_WordPress
+ *
+ * @package Twen Theme
+ * @since 1.0.0
+ *
+ */
---
+++ includes/cpt.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Custom functions added to all projects
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Scope Theme
+ * @since 1.0.0
+ *
+ */
+
+/**
+ * Excerpt Function
+ *
+ * Function used to create CPT for our project.
+ *
+ */
